7d39c411071b5294ba85595a70d6b43c7eebb571
Isabela França <fysabelah@gmail.com>
2024-05-10 18:56:56 -0300

    Adicionar endpoint para Health.

---
+++ src/main/java/com/order/management/system/orderingmicroservice/frameworks/web/HealthWeb.java
@@ -0,0 +1,24 @@
+package com.order.management.system.orderingmicroservice.frameworks.web;
+
+import com.order.management.system.orderingmicroservice.util.health.CustomHealthIndicator;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/health")
+@Tag(name = "Saúde")
+public class HealthWeb {
+
+    @Autowired
+    private CustomHealthIndicator healthIndicator;
+
+    @GetMapping
+    public ResponseEntity<Health> health() {
+        return ResponseEntity.ok(healthIndicator.health());
+    }
+}
